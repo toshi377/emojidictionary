ce34d48c457a4ed90ec43c3c4a194f6d1e8bc023
荒井俊洋 <araitoshihiro@TA-MacBook-Air.local>
2017-07-18 00:17:34 +0900

    Added another view controller

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by 荒井俊洋 on 2017/07/18.
+//  Copyright © 2017年 荒井俊洋. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
